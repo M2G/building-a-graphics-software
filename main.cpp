8e1d54fcf89693e2a315dec9db5d372e6cbe0f3c
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-02 03:17:54 +0200

    feat: rm fckin placeholder from intellij

--- main.cpp
+++ main.cpp
@@ -5,12 +5,9 @@
 extern "C" {
     void init_canvas();
 }
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     init_canvas();
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
