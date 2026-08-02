a12365fd13c5bd2235c7405dd815e0adc08ea19c
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-02 02:04:16 +0200

    feat: add method init_canvas

--- canvas.cpp
+++ canvas.cpp
@@ -9,4 +9,20 @@ static const int CHANNELS = 4; // RGBA
 
 static uint8_t* g_buffer = nullptr;
 
-extern "C" {}
+#ifndef __EMSCRIPTEN__
+   #define EMSCRIPTEN_KEEPALIVE
+#endif
+
+extern "C" {
+   void init_canvas() {
+      size_t size = WIDTH * HEIGHT * CHANNELS;
+      g_buffer = static_cast<uint8_t*>(malloc(size));
+
+      for (size_t i = 0; i < size; i += CHANNELS) {
+         g_buffer[i + 0] = 220;
+         g_buffer[i + 1] = 220;
+         g_buffer[i + 2] = 220;
+         g_buffer[i + 3] = 255;
+      }
+   }
+}
