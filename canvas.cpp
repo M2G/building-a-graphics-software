2ecbad93804e0e50c0b555be78b0c13301917dfc
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-02 02:16:11 +0200

    feat: add stub

--- canvas.cpp
+++ canvas.cpp
@@ -1,7 +1,6 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
-#include <emscripten/emscripten.h>
 
 const int WIDTH = 400;
 static const int HEIGHT = 400;
@@ -9,11 +8,14 @@ static const int CHANNELS = 4; // RGBA
 
 static uint8_t* g_buffer = nullptr;
 
-#ifndef __EMSCRIPTEN__
+#ifdef __EMSCRIPTEN__
+   #include <emscripten/emscripten.h>
+#else
    #define EMSCRIPTEN_KEEPALIVE
 #endif
 
 extern "C" {
+EMSCRIPTEN_KEEPALIVE
    void init_canvas() {
       size_t size = WIDTH * HEIGHT * CHANNELS;
       g_buffer = static_cast<uint8_t*>(malloc(size));
