5266eea9fc2303821b0001e6957672742c357cb4
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-03 23:21:05 +0200

    feat: finalisation of the 'point-in-ellipse' formula

--- src/scene.cpp
+++ src/scene.cpp
@@ -34,6 +34,7 @@ std::optional<int> Scene::hitTest(float x, float y) const {
         switch (obj.type) {
             case ObjectType::RECT: {
                 // https://en.wikipedia.org/wiki/Hit-testing
+                // return not((r1.X + r1.Width < r2.X) or (r1.X > r2.X + r2.Width) or (r1.Y + r1.Height < r2.Y) or (r1.Y > r2.Y + r2.Height));
                 bool inside = (x >= obj.x && x <= obj.x + obj.width &&
                     y >= obj.y && y <= obj.y + obj.height);
                 if (inside) return obj.id;
@@ -41,13 +42,19 @@ std::optional<int> Scene::hitTest(float x, float y) const {
             }
             case ObjectType::ELLIPSE: {
                 // https://www.geeksforgeeks.org/dsa/check-if-a-point-is-inside-outside-or-on-the-ellipse/
-                // // Équation normalisée de l'ellipse : ((x-cx)/rx)^2 + ((y-cy)/ry)^2 <= 1
+                // Équation normalisée de l'ellipse : ((x-cx)/rx)^2 + ((y-cy)/ry)^2 <= 1
+                // ((x - cx) / rx)² + ((y - cy) / ry)² = 1 -> sur le bord de l'ellipse
                 float cx = (obj.x + obj.width) / 2.0f;
                 float cy = (obj.y + obj.height) / 2.0f;
                 float rx = obj.width / 2.0f;
                 float ry = obj.height / 2.0f;
                 // ...
                 if (rx <= 0.0f || ry <= 0.0f) break;
+
+                float dx = (x - cx) / rx;
+                float dy = (y - cy) / ry;
+                if (dx * dx + dy * dy <= 1.0f)
+                    return obj.id;
             }
             case ObjectType::LINE:
             case ObjectType::STROKE:
