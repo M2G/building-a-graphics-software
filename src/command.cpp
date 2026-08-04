35275662970482e6d73ca0b2d4612c59050055ab
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-04 16:56:26 +0200

    feat: create method command

--- src/command.cpp
+++ src/command.cpp
@@ -1,1 +1,15 @@
 #include "command.h"
+
+void CommandHistory::push(Command cmd) {};
+
+bool CommandHistory::undo(Scene& scene){}
+
+bool CommandHistory::redo(Scene& scene){}
+
+bool CommandHistory::canRedo() const {}
+
+bool CommandHistory::canUndo() const {};
+
+void CommandHistory::applyForward(const Command& cmd, Scene& scene){}
+
+void CommandHistory::applyInverse(const Command& cmd, Scene& scene){}
