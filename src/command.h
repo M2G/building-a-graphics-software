4267f6b209da7446486a2aa71c394d00b8e832fe
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-04 03:34:41 +0200

    feat: add command.h and commande.cpp (wip)

--- src/command.h
+++ src/command.h
@@ -20,3 +20,18 @@ struct Command {
     float beforeX = 0, beforeY = 0, beforeWidth = 0, beforeHeight = 0;
     float afterX = 0, afterY = 0, afterWidth = 0, afterHeight = 0;
 };
+
+class CommandHistory {
+public:
+    void push(Command command);
+    bool undo(Scene& scene);
+    bool redo(Scene& scene);
+    bool canUndo() const; // not sure
+    bool canRedo() const; // not sure
+
+private:
+    std::vector<Command> m_undoStack;
+    std::vector<Command> m_redoStack;
+    void applyInverse(const Command& cmd, Scene& scene); // not sure
+    void applyForward(const Command& cmd, Scene& scene); // not sure
+};
